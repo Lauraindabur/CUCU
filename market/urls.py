592324b95f0cd73186481b0cf8bc2de04a894fe6
Lauraindabur <lindaburg@eafit.edu.co>
2026-03-05 15:04:23 -0500

    Corregir errores de merge

--- market/urls.py
+++ market/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import PedidoAceptarAPIView, 
+from .views import PedidoAceptarAPIView
 from .api_views import PedidoCreateAPIView, PedidoDetailAPIView, PublicacionListAPIView
 
 
